05e955496b1ca20d8cdd234b1ee1cfa6ac3aad8b
LoveYuBoBo <956410141@qq.com>
2023-03-25 01:08:52 +0800

    first commit

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,7 +5,7 @@ import "os"
 func LoadConfig() string {
 	_port, present := os.LookupEnv("PORT")
 	if !present {
-		return "127.0.0.1:8083"
+		return "127.0.0.1:80"
 	}
 
 	return "127.0.0.1" + ":" + _port
