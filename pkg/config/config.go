20d658734b0a1115c4602c908cee73c6aa0b8f6e
LoveYuBoBo <956410141@qq.com>
2023-03-25 10:36:59 +0800

    add

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,12 +1,5 @@
 package config
 
-import "os"
-
 func LoadConfig() string {
-	_port, present := os.LookupEnv("PORT")
-	if !present {
-		return "127.0.0.1:8083"
-	}
-
-	return "127.0.0.1" + ":" + _port
+	return "10.0.4.13：8083"
 }
